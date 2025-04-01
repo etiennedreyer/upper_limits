56c13fa7cd2109a545523dd8718dae4d4e7d24b4
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2025-04-01 17:32:01 +0300

    working towards support for 1D case

--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 import numpy as np
+import scipy.optimize as optimize
 import scipy.stats as stats
-from scipy.optimize import minimize_scalar
+import multiprocessing as mp
+from functools import partial
 import yaml
 from tqdm import tqdm
 
 
 class Experiment():
 
-    def __init__(self, config, seed):
+    def __init__(self, config, seed=0):
 
         self.read_config(config)
         self.set_seed(seed)
-        self.set_pdfs()
         self.data = None
         self.q_distributions = {}
 
@@ -21,9 +22,9 @@ class Experiment():
                 config = yaml.safe_load(f)
         self.config = config
         self.mu = config['mu']
-        self.bkg = config['bkg']
+        self.bkg = np.array(config['bkg'])
         self.bkg_sigma = config['bkg_sigma']
-        self.sig = config['sig']
+        self.sig = np.array(config['sig'])
         self.alpha = config['alpha']
         self.limit_type = config['limit_type']
 
@@ -32,45 +33,25 @@ class Experiment():
         np.random.seed(seed)
         print(f"Seed: {seed}")
 
-    def set_pdfs(self):
-        pass
-
-    def generate_data(self, mu, bkg=None):
-        pass
-
-    def get_nll(self, data, mu, bkg=None, do_syst=True):
-        pass
-
-    def get_mu_hat(self, data, bkg=None):
-        pass
-
-    def get_q_mu(self, mu_hat, mu=None):
-        pass
-
-    def get_q_distributions(self, mu_scan):
-        pass
-
-class Experiment_0D(Experiment):
-
-    def __init__(self, config, seed=0):
-        super().__init__(config, seed)
-
     def generate_data(self, mu_inj, n=1):
-        x = np.random.poisson(self.bkg + mu_inj*self.sig, n)
+        x = np.random.poisson(self.bkg + mu_inj*self.sig, size=(n, len(self.bkg)))
         return x
     
-    def nll(self, mu, bu):
-        nll = -np.sum(stats.poisson.pmf(self.data, bu*self.bkg + mu*self.sig))
+    def nll(self, mu, bu, data=None, verbose=False):
+        if data is None:
+            data = self.data
+        nll = -np.sum(data * np.log(bu*self.bkg + mu*self.sig) - (bu*self.bkg + mu*self.sig), axis=1, keepdims=True)
         if self.bkg_sigma > 0:
             nll += 0.5*((bu - 1) / (self.bkg_sigma))**2
-        return nll
+
+        return nll.reshape(-1)
 
     def fit_scalar(self, fn, bounds=None):
 
         if bounds is None:
             bounds = (0, 10)
 
-        fit_res = minimize_scalar(fn, bounds=bounds, method='bounded')
+        fit_res = optimize.minimize_scalar(fn, bounds=bounds, method='bounded')
 
         if fit_res.success:
             if fit_res.x in bounds:
@@ -83,27 +64,33 @@ class Experiment_0D(Experiment):
 
         ### Null hypothesis
         if self.bkg_sigma > 0:
-            nll_fn = lambda bu: self.nll(self.mu, bu)
-            bu_hat = self.fit_scalar(nll_fn)
+            bu_hat = []
+            for data in self.data: ### Unfortunately, not vectorized :(
+                nll_fn = lambda bu: self.nll(self.mu, bu, data.reshape(1, -1))[0]
+                bu_hat.append(self.fit_scalar(nll_fn))
+            bu_hat = np.array(bu_hat).reshape(len(self.data), -1)
         else:
             bu_hat = 1
-        nll_null = self.nll(self.mu, bu_hat)
+        nll_null = self.nll(self.mu, bu_hat, verbose=True)
 
         ### Alternative hypothesis
-        mu_hat = self.data - self.bkg
+        mu_hat = ((self.data - 1*self.bkg) / (self.sig + 1e-8))
         nll_alt = self.nll(mu_hat, 1)
 
         ### q_mu
-        q_mu = 2 * (nll_null - nll_alt) * (mu_hat <= self.mu)
+        q_mu = 2 * (nll_null - nll_alt) * ((mu_hat <= self.mu).reshape(-1))
 
-        return q_mu
+        return_dict = {
+            'q_mu': q_mu,
+            'bu_hat': bu_hat,
+            'nll_null': nll_null,
+            'nll_alt': nll_alt
+        }
+
+        return return_dict
 
     def get_q_distributions(self, mu_scan, n=10000):
         self.q_distributions = {}
         for mu_inj in tqdm(mu_scan):
-            q_mus = []
-            dataset = self.generate_data(mu_inj, n=n)
-            for data in dataset:
-                self.data = data
-                q_mus.append(self.q_mu())
-            self.q_distributions[mu_inj] = np.array(q_mus)
+            self.data = self.generate_data(mu_inj, n=n)
+            self.q_distributions[mu_inj] = self.q_mu()['q_mu']
