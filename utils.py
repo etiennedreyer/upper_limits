dc6dce609b7116ba9174537b6e3fed458bf42313
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2025-04-02 20:36:03 +0300

    progress

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize as optimize
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 import multiprocessing as mp
 from functools import partial
 import yaml
@@ -21,12 +22,13 @@ class Experiment():
             with open(config, 'r') as f:
                 config = yaml.safe_load(f)
         self.config = config
-        self.mu = config['mu']
+        self.mu = config.get('mu', 0.0)
         self.bkg = np.array(config['bkg'])
         self.bkg_sigma = config['bkg_sigma']
         self.sig = np.array(config['sig'])
-        self.alpha = config['alpha']
-        self.limit_type = config['limit_type']
+        self.alpha = config.get('alpha', 0.05)
+        self.limit_type = config.get('limit_type', 'CLs')
+        self.discovery = config.get('discovery', True)
 
     def set_seed(self, seed):
         self.seed = seed
@@ -40,7 +42,11 @@ class Experiment():
     def nll(self, mu, bu, data=None, verbose=False):
         if data is None:
             data = self.data
-        nll = -np.sum(data * np.log(bu*self.bkg + mu*self.sig) - (bu*self.bkg + mu*self.sig), axis=1, keepdims=True)
+        nexp = np.clip(self.bkg + mu*self.sig, 1e-12, None) # avoid log(0)
+
+        nll = -np.sum(data * np.log(nexp) - nexp, axis=1, keepdims=True)
+
+        ### Gaussian penalty on background norm factor
         if self.bkg_sigma > 0:
             nll += 0.5*((bu - 1) / (self.bkg_sigma))**2
 
@@ -78,19 +84,101 @@ class Experiment():
         nll_alt = self.nll(mu_hat, 1)
 
         ### q_mu
-        q_mu = 2 * (nll_null - nll_alt) * ((mu_hat <= self.mu).reshape(-1))
+        q_mu = 2 * (nll_null - nll_alt)
+
+        if self.discovery:
+            if self.mu != 0:
+                raise RuntimeError("Discovery assumes mu=0")
+            ### Eq. 12 of 1007.1727
+            q_mu[mu_hat.flatten() < self.mu] = 0
+            # q_mu *= ((mu_hat >= self.mu).reshape(-1))
+        else:
+            ### Eq. 14 of 1007.1727
+            q_mu[mu_hat.flatten() > self.mu] = 0
 
         return_dict = {
             'q_mu': q_mu,
             'bu_hat': bu_hat,
+            'mu_hat': mu_hat,
             'nll_null': nll_null,
             'nll_alt': nll_alt
         }
 
         return return_dict
 
-    def get_q_distributions(self, mu_scan, n=10000):
+    def get_q_distributions(self, mu_scan=None, n=10000):
+        if mu_scan is None:
+            mu_scan = np.linspace(0, 5, 40)
+        ### make sure we have the most interesting mu values
+        if 0.0 not in mu_scan:
+            mu_scan = np.concatenate(([0.0], mu_scan))
+        if self.mu not in mu_scan:
+            mu_scan = np.concatenate(([self.mu], mu_scan))
+        mu_scan = np.sort(mu_scan)
+
         self.q_distributions = {}
         for mu_inj in tqdm(mu_scan):
             self.data = self.generate_data(mu_inj, n=n)
             self.q_distributions[mu_inj] = self.q_mu()['q_mu']
+    
+    def get_upper_limit(self, q_hat):
+
+        if self.q_distributions == {}:
+            raise RuntimeError("No q distributions found. Run get_q_distributions() first.")
+
+        def get_p_values(dist, q_hat):
+            p_left  = np.sum(dist <= q_hat, axis=-1) / dist.shape[-1]
+            p_right = 1 - p_left
+            return p_left, p_right
+
+        # sort dictionary by key (q_inj)
+        self.q_distributions = dict(sorted(self.q_distributions.items()))
+
+        # create a 2D array of q distributions
+        mu_inj_arr = np.array(list(self.q_distributions.keys()))
+        q_dist_arr = np.stack(list(self.q_distributions.values()), axis=0)
+
+        # get the p-value for each q_inj in vectorized way
+        p_left, p_right = get_p_values(q_dist_arr, q_hat)
+
+        # get the right-tail p-value for null (mu_inj = mu)
+        mask = mu_inj_arr == self.mu ; assert mask.sum() == 1, f"More than one mu_inj = mu found: {mu_inj_arr[mask_null]}"
+        p_mu_null = p_right[mask][0]
+
+        # get the left-tail p-value for alternate (mu_inj != mu)
+        mask = mu_inj_arr != self.mu
+        p_mu_alt= p_left[mask]
+        mu_inj_alt = mu_inj_arr[mask]
+
+        CLs = p_mu_alt / (1 - p_mu_null)
+
+        # Find where CLs crosses alpha
+        # assert np.all(np.diff(CLs) < 0), "CLs is not monotonic"
+        # assert mask.sum() > 0, f"CLs never crosses alpha={self.alpha}"
+        mu_inj_where_CLs_at_alpha = np.interp(self.alpha, CLs[::-1], mu_inj_alt[::-1])
+
+        return_dict = {
+            'mu_upper_limit': mu_inj_where_CLs_at_alpha,
+            'p_mu_null': p_mu_null,
+            'p_mu_alt': p_mu_alt,
+            'mu_inj_alt': mu_inj_alt,
+            'CLs': CLs,
+        }
+
+        return return_dict
+    
+
+    def plot_single_limit(self, result):
+
+        fig, ax = plt.subplots(1, 1, figsize=(5, 3), dpi=150)
+
+        ax.axhline(y=result['p_mu_null'], label='$p_\\mu, \\mu\'=\\mu$', color='b', ls='-.')
+        ax.plot(result['mu_inj_alt'], result['p_mu_alt'], label='$p_\\mu, \\mu\'\\neq \\mu$', color='r', ls='--')
+        ax.plot(result['mu_inj_alt'], result['CLs'], label='$CL_s$', ls='-', color='purple')
+        ax.axhline(y=self.alpha, color='k', ls=':', label='$\\alpha$')
+        ax.scatter(result['mu_upper_limit'], self.alpha, color='k', marker='o', label='$\\mu_{UL}$', facecolors='none', edgecolors='k')
+        ax.set_xlabel('$q_{inj}$')
+        ax.set_ylabel('$p_{\\mu=' + f'{self.mu:.2f}' + '}$')
+        ax.legend()
+        
+        return fig, ax
