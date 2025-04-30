42281dbe2177d8cdbb5a6575f913b6659e4899bb
Etienne Dreyer <etienne.dreyer@weizmann.ac.il>
2025-04-30 10:58:33 +0300

    moving away from discovery mode. Got q_mu based limits working

--- utils.py
+++ utils.py
@@ -15,7 +15,6 @@ class Experiment():
         self.read_config(config)
         self.set_seed(seed)
         self.data = None
-        self.q_distributions = {}
 
     def read_config(self, config):
         if isinstance(config, str):
@@ -29,6 +28,7 @@ class Experiment():
         self.alpha = config.get('alpha', 0.05)
         self.limit_type = config.get('limit_type', 'CLs')
         self.discovery = config.get('discovery', True)
+        self.two_sided = config.get('two_sided', False)
 
     def set_seed(self, seed):
         self.seed = seed
@@ -66,23 +66,26 @@ class Experiment():
         else:
             raise RuntimeError("Fit failed")
 
-    def q_mu(self, data=None):
+    def q_mu(self, data=None, mu=None):
 
         if data is None:
             data = self.data
+
+        if mu is None:
+            mu = self.mu
 
         ### Null hypothesis
         if self.bkg_sigma > 0:
             bu_hat = []
             for data_i in data: ### Unfortunately, not vectorized :(
-                nll_fn = lambda bu: self.nll(self.mu, bu, data=data_i.reshape(1, -1))[0]
+                nll_fn = lambda bu: self.nll(mu, bu, data=data_i.reshape(1, -1))[0]
                 bu_hat.append(self.fit_scalar(nll_fn))
                 # if np.abs(bu_hat[-1] - 1) < 0.5:
                 #     print(f"WARNING: bu_hat is close to 1: {bu_hat[-1]}")
             bu_hat = np.array(bu_hat).reshape(len(data), -1)
         else:
             bu_hat = 1
-        nll_null = self.nll(self.mu, bu_hat, data=data, verbose=True)
+        nll_null = self.nll(mu, bu_hat, data=data, verbose=True)
 
         ### Alternative hypothesis
         mu_hat = ((data - 1*self.bkg) / (self.sig + 1e-8))
@@ -91,16 +94,24 @@ class Experiment():
         ### q_mu
         q_mu = 2 * (nll_null - nll_alt)
 
+        factor = -1 if self.two_sided else 0
+
+        mu_hat = mu_hat.flatten()
         if self.discovery:
-            if self.mu != 0:
+            if mu != 0:
                 raise RuntimeError("Discovery assumes mu=0")
             ### Eq. 12 of 1007.1727
-            q_mu[mu_hat.flatten() < self.mu] = 0
+            q_mu[mu_hat < mu] *= factor
         else:
             ### Eq. 14 of 1007.1727
-            q_mu[mu_hat.flatten() > self.mu] = 0
+            if len(mu_hat) == 1:
+                if mu_hat[0] > mu:
+                    q_mu *= factor
+            else:
+                q_mu[mu_hat > mu] *= factor
 
         return_dict = {
+            'mu': mu,
             'q_mu': q_mu,
             'bu_hat': bu_hat,
             'mu_hat': mu_hat,
@@ -110,68 +121,79 @@ class Experiment():
 
         return return_dict
 
-    def get_q_distributions(self, mu_scan=None, n=100000):
-        if mu_scan is None:
-            mu_scan = np.linspace(self.mu, (self.mu + 1)*10, 20)
-        ### make sure we have the most interesting mu values
-        if 0.0 not in mu_scan:
-            mu_scan = np.concatenate(([0.0], mu_scan))
-        if self.mu not in mu_scan:
-            mu_scan = np.concatenate(([self.mu], mu_scan))
-        mu_scan = np.sort(mu_scan)
+    def get_q_distribution(self, mu_inj, mu=None, n=100000):
 
-        self.q_distributions = {}
-        for mu_inj in tqdm(mu_scan):
-            toy_data = self.generate_data(mu_inj, n=n)
-            self.q_distributions[mu_inj] = self.q_mu(data=toy_data)['q_mu']
+        if mu is None:
+            mu = self.mu
+        toy_data = self.generate_data(mu_inj, n=n)
+        results = self.q_mu(data=toy_data, mu=mu)
+        return results['q_mu'], results
     
-    def get_upper_limit(self, q_hat=None):
+    def get_upper_limit(self, mu_hat=None, mu_scan=None, n=100000):
 
-        if q_hat is None:
-            fit_result = self.q_mu(self.data)
-            mu_hat, q_hat = fit_result['mu_hat'], fit_result['q_mu']
+        if mu_hat is None:
+            fit_result = self.q_mu(self.data, mu=0)
+            mu_hat, q_0 = fit_result['mu_hat'], fit_result['q_mu']
 
-        if self.q_distributions == {}:
-            raise RuntimeError("No q distributions found. Run get_q_distributions() first.")
+        if mu_scan is None:
+            mu_scan = np.linspace(mu_hat[0], 10*(mu_hat[0]+1), 20)
+        else:
+            assert np.diff(mu_scan).min() > 0, "mu_scan must be monotonically increasing"
 
-        def get_p_values(dist, q_hat):
-            p_left  = np.sum(dist <= q_hat, axis=-1) / dist.shape[-1]
-            p_right = 1 - p_left
-            return p_left, p_right
+        # get q hat values for each mu
+        q_hats = []
+        for mu in mu_scan:
+            q_hats.append(self.q_mu(data=self.data, mu=mu)['q_mu'])
+        q_hats = np.array(q_hats)
 
-        # sort dictionary by key (q_inj)
-        self.q_distributions = dict(sorted(self.q_distributions.items()))
+        # get q distributions for each mu=mu_inj
+        q_distributions_splusb = {}
+        q_distributions_bonly  = {}
+        for mu in tqdm(mu_scan, desc="Getting q distributions"):
+            q_distributions_splusb[mu], _ = self.get_q_distribution(mu_inj=mu, mu=mu, n=n)
+
+            if self.limit_type in ['CLs']:
+                for mu in tqdm(mu_scan, desc="Getting q distributions"):
+                    q_distributions_bonly[mu], _ = self.get_q_distribution(mu_inj=0, mu=mu, n=n)
+            else:
+                q_distributions_bonly[mu] = np.zeros_like(q_distributions_splusb[mu])
 
         # create a 2D array of q distributions
-        mu_inj_arr = np.array(list(self.q_distributions.keys()))
-        q_dist_arr = np.stack(list(self.q_distributions.values()), axis=0)
+        q_array_splusb = np.stack(list(q_distributions_splusb.values()), axis=0)
+        q_array_bonly  = np.stack(list(q_distributions_bonly.values()), axis=0)
+
+        def get_p_value(dist, q_hat, right=True):
+            if right:
+                p = np.sum(dist > q_hat, axis=-1) / dist.shape[-1]
+            else:
+                p = np.sum(dist < q_hat, axis=-1) / dist.shape[-1]
+            return p
 
         # get the p-value for each q_inj in vectorized way
-        p_left, p_right = get_p_values(q_dist_arr, q_hat)
+        p_splusb = get_p_value(q_array_splusb, q_hats)
+        if self.limit_type in ['CLs']:
+            p_bonly = get_p_value(q_array_bonly, q_hats, right=False)
+        else:
+            p_bonly = np.zeros_like(p_splusb)
 
-        # get the right-tail p-value for null (mu_inj = mu)
-        mask = mu_inj_arr == self.mu ; assert mask.sum() == 1, f"More than one mu_inj = mu found: {mu_inj_arr[mask]}"
-        p_mu_null = p_right[mask][0]
-
-        # get the left-tail p-value for alternate (mu_inj != mu)
-        mask = mu_inj_arr != self.mu
-        p_mu_alt= p_left[mask]
-        mu_inj_alt = mu_inj_arr[mask]
-
-        CLs = p_mu_alt / (1 - p_mu_null)
+        CLs = p_splusb / (1 - p_bonly)
 
         # Find where CLs crosses alpha
         # assert np.all(np.diff(CLs) < 0), "CLs is not monotonic"
         if (CLs < self.alpha).sum() == 0:
             print(f"WARNING: CLs never crosses alpha={self.alpha}! Minimum CLs: {CLs.min()}")
-        mu_inj_where_CLs_at_alpha = np.interp(self.alpha, CLs[::-1], mu_inj_alt[::-1])
+        mu_inj_where_CLs_at_alpha = np.interp(self.alpha, CLs[::-1], mu_scan[::-1])
 
         return_dict = {
+            'mu_hat': mu_hat,
             'mu_upper_limit': mu_inj_where_CLs_at_alpha,
-            'p_mu_null': p_mu_null,
-            'p_mu_alt': p_mu_alt,
-            'mu_inj_alt': mu_inj_alt,
+            'p_splusb': p_splusb,
+            'p_bonly': p_bonly,
+            'q_hats': q_hats,
+            'mu_scan': mu_scan,
             'CLs': CLs,
+            'q_distributions_splusb': q_distributions_splusb,
+            'q_distributions_bonly': q_distributions_bonly,
         }
 
         return return_dict
@@ -181,9 +203,9 @@ class Experiment():
 
         fig, ax = plt.subplots(1, 1, figsize=(5, 3), dpi=150)
 
-        ax.axhline(y=result['p_mu_null'], label='$p_\\mu, \\mu\'=\\mu$', color='b', ls='-.')
-        ax.plot(result['mu_inj_alt'], result['p_mu_alt'], label='$p_\\mu, \\mu\'\\neq \\mu$', color='r', ls='--')
-        ax.plot(result['mu_inj_alt'], result['CLs'], label='$CL_s$', ls='-', color='purple')
+        ax.plot(result['mu_scan'], result['p_bonly'], label='$p_{b}$', color='b', ls='-.')
+        ax.plot(result['mu_scan'], result['p_splusb'], label='$p_{s+b}$', color='r', ls='--')
+        ax.plot(result['mu_scan'], result['CLs'], label='$CL_s$', ls='-', color='purple')
         ax.axhline(y=self.alpha, color='k', ls=':', label='$\\alpha$')
         ax.scatter(result['mu_upper_limit'], self.alpha, color='k', marker='o', label='$\\mu_{UL}$', facecolors='none', edgecolors='k')
         ax.set_xlabel('$\\mu_{inj}$')
